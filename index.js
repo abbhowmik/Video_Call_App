6737c8766b937a8e1ea0971bd5d2fb8a124a81a5
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2021-09-10 00:16:50 +0530

    minor version pushing

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Application is Running....");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+server.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
